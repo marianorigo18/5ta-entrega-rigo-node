019546cc5f23545b92caad8f22d34f781901c8c1
marianorigo18 <marianowagnerrigo@gmail.com>
2023-05-30 23:55:51 -0300

    fix commit

--- src/server.js
+++ src/server.js
@@ -10,6 +10,6 @@ app.use(express.urlencoded({extended: true}));
 app.use("/api/products", routerProducts)
 app.use("/api/carts", routerCarts)
 
-app.listen(8081, ()=>{
+app.listen(8080, ()=>{
     console.log("escuchando")
 })
