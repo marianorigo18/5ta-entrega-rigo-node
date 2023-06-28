e5262233820c0c6cf055d5f72ebac30336551fac
marianorigo18 <marianowagnerrigo@gmail.com>
2023-06-27 21:19:17 -0300

    fix commit

--- src/daos/mongodb/CartManager.js
+++ src/daos/mongodb/CartManager.js
@@ -5,9 +5,12 @@ export default class ManagerCarts {
   connection = mongoose.connect("mongodb+srv://marianowagnerrigo18:Marawarigo3360@cluster0.xjgkqac.mongodb.net/")
 
   consultarCarts = async () => {
+    const result = await cartModel.create({products: []})
+    return result
   };
 
   crearCart = async () => {
+    
   };
 
   consultarCartPorId = async (id) => {
